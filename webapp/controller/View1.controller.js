4e666969088a746378ad742b9b56d314466c4270
Ramya R <ramya.r@signiwis.com>
2019-06-28 06:51:29 +0000

    commit login page

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,8 +4,22 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("demo_git.demo_git.controller.View1", {
-		onInit: function () {
+		onLoginTap: function () {
+			var uid = this.getView().byId("uid").getValue();
+			var pasw = this.getView().byId("pasw").getValue();
+			if (uid === "Admin" && pasw === "123") {
+				sap.m.MessageToast.show(uid + " Logged In");
+				this.getView().byId("uid").setValue("");
+				this.getView().byId("pasw").setValue("");
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("admin");
+			} else if (uid !== "" && pasw !== "") {
+				sap.m.MessageToast.show("Authentication failed");
+			} else {
+				sap.m.MessageToast.show("Either User ID or Password is empty");
+			}
 
 		}
+
 	});
 });
